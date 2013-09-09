100ea587e6d456766d36cfe09ac9e48345aa296d
Josh <josh@magicmoments.com>
2013-09-09 15:45:39 -0400

    added code for creating a uniform way to reference external objects

---
+++ ExampleReference.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace JoshCodes.Persistence.Azure.Storage.Testing.Unit
+{
+    class ExampleReference : AzureObjectWrapper<ExampleReference.Entity>
+    {
+        public ExampleReference(CloudTableClient tableClient, Example example)
+            : base(tableClient, "ExampleReference", (out string partitionKey, out string rowKey) =>
+                {
+                    rowKey = Guid.NewGuid().ToString();
+                    partitionKey = example.Int.ToString();
+                    return new ExampleReference.Entity()
+                    {
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now,
+                    };
+                })
+        {
+            Storage.IdExample = SetReferencedObject(example);
+        }
+
+        public Example Example
+        {
+            get
+            {
+                var example = GetReferencedObject<Example.Entity, Example>(
+                    Storage.IdExample,
+                    (entity) => new Example(_tableClient, entity));
+                return example;
+            }
+        }
+
+        public class Entity : Storage.Entity
+        {
+            public string IdExample { get; set; }
+        }
+    }
+}
---
+++ ReferenceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoshCodes.Persistence.Azure.Storage
+{
+    [TestClass]
+    public class ReferenceTests
+    {
+        [TestMethod]
+        public void TestReference()
+        {
+            var tableClient = JoshCodes.Persistence.Azure.Storage.Settings.StorageAccount().CreateCloudTableClient();
+            var entityWrapper = new Testing.Unit.Example(tableClient, Guid.NewGuid().ToString(), "ConcurrentModification", -1, 0.0, "foo", null);
+            var entityWrapperWithRefernce = new Testing.Unit.ExampleReference(tableClient, entityWrapper);
+
+            var referencedEntityWrapper = entityWrapperWithRefernce.Example;
+            Assert.AreEqual(entityWrapper.Int, referencedEntityWrapper.Int);
+        }
+    }
+}
