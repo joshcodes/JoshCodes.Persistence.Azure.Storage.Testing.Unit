d52acc47386e2a2a9e5dc9ddfd393e65145cc89a
Josh <josh@magicmoments.com>
2013-11-12 13:26:44 -0500

    added KVP store implementation

---
+++ KvpTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using JoshCodes.Persistence.Azure.Storage;
+
+namespace JoshCodes.Persistence.Azure.Storage.Testing.Unit
+{
+    [TestClass]
+    public class KvpTests
+    {
+        [TestMethod]
+        public void TestCreateAndGet()
+        {
+            // Setup storage
+            var tableClient = JoshCodes.Persistence.Azure.Storage.Settings.StorageAccount().CreateCloudTableClient();
+            var store = new KvpStore(tableClient);
+
+            // Setup test values
+            var container =  "KvpTests.TestGetAndSet";
+            var key = Guid.NewGuid().ToString("N");
+            var val = Guid.NewGuid().ToString();
+            
+            // Test empty get
+            var getVal = store.Get(container, key);
+            Assert.IsNull(getVal);
+
+            // Test Create
+            store.Create(container, key, val);
+
+            // Test Get
+            getVal = store.Get(container, key);
+            Assert.AreEqual(val, getVal);
+        }
+    }
+}
